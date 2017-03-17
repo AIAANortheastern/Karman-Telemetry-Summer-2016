646c46e62b5979351b8a7ae50bf36822ea0151e4
ZachMarcus <zachary.j.marcus@gmail.com>
2017-03-16 21:08:45 -0400

    Change demo code for readability, python3, correct and safe file usage

--- NewerHotness/testXbee.py
+++ NewerHotness/testXbee.py
@@ -1,40 +1,30 @@
-#from xbee import XBee
+
 import serial
-from struct import *
+from struct import unpack
 import os.path
 
 
 PORT = 'COM5'
 BAUD_RATE = 57600
 
-# Open serial port
-ser = serial.Serial(PORT, BAUD_RATE)
+# Safely open serial port
+with serial.Serial(PORT, BAUD_RATE) as ser:
+    fileName = "RecvData"
+    i = 0
+    # Name the file using the next available number to differentiate it
+    # Checks to see if the next numbered filename already exists
+    while os.path.isfile(fileName + str(i) + ".csv"):
+        i = i + 1
 
-# Create API object
-#xbee = XBee(ser)
-
-filename = "RecvData0"
-i = 0
-while True == os.path.isfile(filename + ".csv") :
-      filename = filename[:-1] + str(i)
-      i = i + 1
-
-filename += ".csv"
-
-dataFile = open(filename, 'w');
-
-# Continuously read and print packets
-while True:
-    try:
-        #response = xbee.wait_read_frame()
-        if(ser.inWaiting() >= 52):
-            response = ser.read(52)
-            temp1, temp2, temp3, temp10dof, alt_press, alt_strat, alt_gps, lat, lon, accx, accy, accz, flags, pad = unpack('fffffiffffffhh', response)
-            print "Temp: ", temp1 , "accel: ", accx
-            s = str(temp1) + "," + str(temp2) + "," + str(temp3) + "," + str(temp10dof) + "," + str(alt_press) + "," + str(alt_strat) + "," + str(alt_gps) + "," + str(lat) + "," + str(lon) + "," + str(accx) + "," + str(accy) + "," + str(accz) + "," + str(flags) + "\n"
-            dataFile.write(s)
-    except KeyboardInterrupt:
-        break
-        
-ser.close()
-dataFile.close();
+    with open(fileName + str(i) + ".csv") as dataFile:
+        # Continuously read and print packets
+        while True:
+            if ser.inWaiting() >= 52:
+                response = ser.read(52)
+                temp1, temp2, temp3, temp10dof, alt_press, alt_strat, \
+                  alt_gps, lat, lon, accx, accy, accz, flags, \
+                  pad = unpack('fffffiffffffhh', response)
+                print("Temp: ", temp1 , "accel: ", accx)
+                dataFile.write(",".join(map(str, [temp1, temp2, temp3, \
+                  temp10dof, alt_press, alt_strat, alt_gps, lat, lon, accx, \
+                  accy, accz, flags])) + "\n")
